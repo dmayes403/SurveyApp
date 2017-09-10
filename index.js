f8400f014b61c32aef11f0f64fa957bd48a4e4aa
dmayes403 <davidrmayes@gmail.com>
2017-09-10 15:31:19 -0600

    passport oauth implemented

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require('express');
 // this import style is using common js modules ^^ instead of import express from 'express' like we see in js files
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-passport.use(new GoogleStrategy())
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('access token: ', accessToken);
+        console.log('refresh token: ', refreshToken);
+        console.log('profile: ', profile);
+        // ^^ this callback is called instantly when the user is sent back to our server
+    })
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+        // specifies what info we want from google about the user
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+// ^^ this is different this time because there is a 'code' on the url
+
 
 // app.get('/', (req, res) => {
 //     res.send({ hi: 'hey johnny!' });
