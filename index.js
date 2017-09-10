8f583cf5dd6404d69deb730038dd1cbc1be4d0a1
dmayes403 <davidrmayes@gmail.com>
2017-09-10 00:20:06 -0600

    setting up project for heroku

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// this import style is using common js modules ^^ instead of import express from 'express' like we see in js files
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+})
+// ^^ this creates a new route handler that is watching for http requests. 
+// we also have available get, put, post, patch, delete.
+// Res is what's being sent back to the front end
+
+const PORT = process.env.PORT || 5000;
+// ^^ environment variables from heroku or dev port if heroku port is not defined
+app.listen(PORT);
+// this line is express telling *node* what port to listen to
