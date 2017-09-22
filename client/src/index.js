791f0652891f3c5ade97d2916a8db9a709200abe
dmayes403 <davidrmayes@gmail.com>
2017-09-21 22:20:17 -0600

    setting up provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
-    <App />, document.querySelector('#root')
+    <Provider store={store}><App /></Provider>,
+    // ^^ this is the glue to the redux store
+    document.querySelector('#root')
     // ^^ this takes the App component, and renders it to the div with an id of "root" inside
     // the public/index.html file
 );
