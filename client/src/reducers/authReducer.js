235db49f9067443decd199a4899588624ab4b8cd
dmayes403 <davidrmayes@gmail.com>
2017-09-23 13:34:23 -0600

    creating combine reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,7 @@
+export default function(state = null, action) {
+    // ^^ it's important to initialize state as null
+    switch(action.type) {
+        default:
+            return state;
+    }
+} 
