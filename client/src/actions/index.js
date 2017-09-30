2c8d5e0159e9c1aedbcfd9f01f63eec3e30ce5e7
dmayes403 <davidrmayes@gmail.com>
2017-09-30 14:23:59 -0600

    creating endpoint for stripe checkout

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
-        // if fetchUser is calling a function, redux-thunk will *automatically*
-        // pass in *dispatch* as an argument
-        const res = await axios.get('/api/current_user');
-        dispatch({ type: FETCH_USER, payload: res.data });
-    };
+    // if fetchUser is calling a function, redux-thunk will *automatically*
+    // pass in *dispatch* as an argument
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe', token);
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
