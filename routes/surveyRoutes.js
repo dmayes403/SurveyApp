2b1fca997854c4b44abe2a22a53b203149545a80
dmayes403 <davidrmayes@gmail.com>
2017-10-09 22:06:09 -0600

    Making fixes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -25,5 +25,6 @@ module.exports = app => {
         const mailer = new Mailer(survey, surveyTemplate(survey));
         // ^^ creating a new instance of the mailer class
         mailer.send();
+        // ^^ working on fixing API
     });
 };
