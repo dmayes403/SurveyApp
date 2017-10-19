16305cd105229b596a70445035e781cc338f225d
dmayes403 <davidrmayes@gmail.com>
2017-10-18 22:23:33 -0600

    working on review submission

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -31,7 +31,8 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+                    {/* ^^ don't put paranthesis on the end of function because then it will be called the second it's renderFields */}
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">
                         Cancel
@@ -49,7 +50,7 @@ class SurveyForm extends Component {
 
 function validate(values) {
     const errors = {};
-    
+
     errors.emails = validateEmails(values.emails || '');
     // ^^ the or statement takes care of when there are no emails provided
 
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,31 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {showFormReview: false}
+    // }
+    // 100% the same as the statement below
+    state = { showFormReview: false };
+    // ^^ this is component level state. Simple way provided by create-react-app
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />;
+        }
+
+        return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
+        // ^^ this creates a function called onSurveySubmit on the state object
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
