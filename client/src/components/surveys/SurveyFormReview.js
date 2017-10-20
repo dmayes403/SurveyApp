64a8f97c14020075fee5f9bb7655b85ead46488f
dmayes403 <davidrmayes@gmail.com>
2017-10-19 21:16:04 -0600

    creating action creator for survey submission

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,18 +1,35 @@
 // SurveyFormreview shows users their form inputs for review
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
-    // ^^ onCancel is destructured from this.props.onCancel. formValues comes from setting it on state in mapStateToProps
+    // ^^ onCancel is destructured from this.props.onCancel. 
+    // formValues comes from setting it on state in mapStateToProps
+    const reviewFields = _.map(formFields, ({ name, label }) => {
+                                            // ^^ this is pulling name and label off of the field object
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>
+        );
+    })
+
     return (
         <div>
             <h5>Please confirm your entries</h5>
-            <div>
-
-            </div>
-            <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+            {reviewFields}
+            {/* ^^ because we're producing a list, each div must have a key */}
+            <button className="yellow white-text darken-3 btn-flat" onClick={onCancel}>
                 Back
+            </button>
+            <button className="green white-text btn-flat right">
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
