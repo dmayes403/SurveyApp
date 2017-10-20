1f45e6cf2379341aa3cee511ef9845491202920f
dmayes403 <davidrmayes@gmail.com>
2017-10-19 21:04:13 -0600

    created formFields file

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,17 +4,11 @@ import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import _ from 'lodash';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' }
-];
+import formFields from './formFields.js'
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             // ^^ this pull label and name off of the field variable instead of field.lable or field.name
             return (
                 <Field 
@@ -54,7 +48,7 @@ function validate(values) {
     errors.emails = validateEmails(values.emails || '');
     // ^^ the or statement takes care of when there are no emails provided
 
-    _.each(FIELDS, ({ name }) => {
+    _.each(formFields, ({ name }) => {
         if (!values[name]) {
             errors[name] = `You must have a ${name}!`
         }
@@ -71,5 +65,7 @@ export default reduxForm({
     validate,
     // validate: validate,
     // ^^ the value 'validate' is referring to the function validate above
-    form: 'surveyForm'
+    form: 'surveyForm',
+    // ^^ this labels this form as surveyForm which can later be pulled off of props
+    destroyOnUnmount: false
 })(SurveyForm);
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,16 +1,26 @@
 // SurveyFormreview shows users their form inputs for review
 import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
 
-const SurveyFormReview = ({ onCancel }) => {
-    // ^^ onCancel is destructured from this.props.onCancel
+const SurveyFormReview = ({ onCancel, formValues }) => {
+    // ^^ onCancel is destructured from this.props.onCancel. formValues comes from setting it on state in mapStateToProps
     return (
         <div>
             <h5>Please confirm your entries</h5>
-            <button className="yellow dark-3 btn-flat" onClick={onCancel}>
+            <div>
+
+            </div>
+            <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
         </div>
     );
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+    return { formValues: state.form.surveyForm.values };
+    // ^^ setting surveyForm values on the formValues prop of state
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
